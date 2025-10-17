4064eac20e2490eab330776a2618d1e763efe712
timgusev777 <timgusev785@gmail.com>
2025-10-17 20:27:16 +0300

    Update guessing_numbers (lab 2).py

--- guessing_numbers (lab 2).py
+++ guessing_numbers (lab 2).py
@@ -1,3 +1,4 @@
+import unittest
 
 def lin_guess_number(number, start, finish):
     steps = 0
@@ -22,21 +23,21 @@ def bin_guess_number(number, start, finish):
         lst.append(i)
 
     low = 0
-    mid = len(lst) // 2
     high = len(lst) - 1
 
-    while lst[mid] != number and low <= high:
-        if number > lst[mid]:
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == number:
+            return (number, steps + 1)
+
+        elif number > lst[mid]:
             low = mid + 1
             steps += 1
         else:
             high = mid - 1
             steps += 1
-        mid = (low + high) // 2
-    if low > high:
-        return (None, steps)
-    else:
-        return (number, steps)
+
+    return(None, steps)
 
 
 def guess_number():
@@ -52,7 +53,20 @@ def guess_number():
     if var == 2:
         target, steps_count = bin_guess_number(number, start, finish)
 
-    return "Искомое число: " + str(target), "количество шагов: " + str(steps_count)
+    return target, steps_count
 
 
-print(guess_number())
+class TestMath(unittest.TestCase):
+    def test_lin_guess_number_1(self):
+        self.assertEqual(lin_guess_number(5, 1, 10), (5, 5))
+
+    def test_lin_guess_number_2(self):
+        self.assertEqual(lin_guess_number(1, 1, 3), (1, 1))
+
+    def test_bin_guess_number_1(self):
+        self.assertEqual(bin_guess_number(5, 1, 10), (5, 1))
+
+    def test_bin_guess_number_2(self):
+        self.assertEqual(bin_guess_number(1, 1, 10), (1, 3))
+
+unittest.main(argv=[''], verbosity=2, exit=False)
